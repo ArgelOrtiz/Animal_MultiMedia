e1bd1486489029fe2a5c2a058442852844ebc719
ArgelOrtiz <7624000argel>
2019-10-29 08:19:52 -0600

    (Droid):MediaPlayer implemented

--- app/src/main/java/com/tec/aoamultimedia/MainActivity.java
+++ app/src/main/java/com/tec/aoamultimedia/MainActivity.java
@@ -2,7 +2,10 @@ package com.tec.aoamultimedia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initComponents();
+    }
+
+    private void initComponents(){
+        Button catButton    = findViewById(R.id.catMainButton);
+        Button lionButton   = findViewById(R.id.lionMainButton);
+
+        catButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer mp  = MediaPlayer.create(getApplicationContext(),R.raw.gato);
+                mp.start();
+
+            }
+        });
+
+        lionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer mp  = MediaPlayer.create(getApplicationContext(),R.raw.leon);
+                mp.start();
+            }
+        });
     }
 }
